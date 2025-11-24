ea900a702f7d81262132725c076df36ec6b5d715
JustinMillett54 <justin.millett@redfoxgroup.io>
2025-11-23 18:11:49 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,17 @@
+# versions.tf – Defines Terraform version and providers
+# Options: Update provider versions as new releases come out (check registry.terraform.io)
+# If adding VM automation, add providers like bpg/proxmox or hashicorp/vsphere here
 terraform {
-  required_version = ">= 1.8"
+  required_version = ">= 1.8"  # Minimum Terraform version – upgrade for new features like import blocks
 
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.40"
+      version = "~> 5.40"  # Locks to latest v5 series – check for updates quarterly
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"  # For generating tunnel secrets – optional but secure
     }
   }
 }
